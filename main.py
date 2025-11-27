38962e0c5b25e4f4f5c357cb7b39637c212809ee
Ashita <ashitasharma2007@gmail.com>
2025-11-27 18:21:04 +0530

    Adding more functions :3

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ class DrawInformation:
     GRAY = (128, 128, 128)
     BACKGROUND_COLOR = WHITE
 
+    TOP_PAD = 150
+    SIDE_PAD = 100
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
@@ -17,5 +19,21 @@ class DrawInformation:
         pygame.display.set_caption("Algorithm Visualizer")
         self.set_list(lst)
     def set_list(self, lst):
-        pass
-    
+        self.lst = lst
+        self.min_val = min(list)
+        self.max_val = max(list)
+
+        self.block_width = round((self.width - self.SIDE_PAD)/len(lst))
+        self.block_height = round((self.height - self.TOP_PAD)/(self.max_val - self.min_val))
+        self.start_x = self.SIDE_PAD // 2
+def generate_starting_list(n, min_val, max_val):
+    lst = []
+    for i in range(n):
+        lst.append(random.randint(min_val, max_val))
+    return lst
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    while run:
+        clock.tick(60)
