58c3eaa87d8c6d755320bdbaf53689877aadb650
Ashita <ashitasharma2007@gmail.com>
2025-11-27 18:01:07 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+pygame.init()
+
+class DrawInformation:
+    BLACK = (0, 0, 0)
+    WHITE = (255, 255, 255)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    GRAY = (128, 128, 128)
+    BACKGROUND_COLOR = WHITE
+
+    def __init__(self, width, height, lst):
+        self.width = width
+        self.height = height
+        self.window = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Algorithm Visualizer")
+        self.set_list(lst)
+    def set_list(self, lst):
+        pass
+    
