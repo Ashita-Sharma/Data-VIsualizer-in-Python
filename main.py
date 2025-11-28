ff91ed2b75651825f4aed5aaaf576865df6b6a90
Ashita <ashitasharma2007@gmail.com>
2025-11-28 12:43:56 +0530

    added random data generation function and drawing basic stuff

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ class DrawInformation:
     WHITE = (255, 255, 255)
     RED = (255, 0, 0)
     GREEN = (0, 255, 0)
-    GRAY = (128, 128, 128)
     BACKGROUND_COLOR = WHITE
-
+    GRADIENTS = [ (128,128,128), (160,160,160), (192,192,192)]
     TOP_PAD = 150
     SIDE_PAD = 100
+
+    FONT = pygame.font.SysFont('comicsans',20)
+    LARGE_FONT = pygame.font.SysFont('comicsans', 40)
+
+
     def __init__(self, width, height, lst):
         self.width = width
         self.height = height
@@ -20,8 +24,8 @@ class DrawInformation:
         self.set_list(lst)
     def set_list(self, lst):
         self.lst = lst
-        self.min_val = min(list)
-        self.max_val = max(list)
+        self.min_val = min(lst)
+        self.max_val = max(lst)
 
         self.block_width = round((self.width - self.SIDE_PAD)/len(lst))
         self.block_height = round((self.height - self.TOP_PAD)/(self.max_val - self.min_val))
@@ -32,8 +36,55 @@ def generate_starting_list(n, min_val, max_val):
         lst.append(random.randint(min_val, max_val))
     return lst
 
+def draw(draw_info):
+    draw_info.window.fill(draw_info.BACKGROUND_COLOR)
+    controls = draw_info.FONT.render("R - Reset | A - Ascending | D - Descending | Space - Start",1,draw_info.BLACK)
+    draw_info.window.blit(controls, (draw_info.width/2 - controls.get_width()/2, 5))
+    sorts = draw_info.FONT.render("B - Bubble Sort | I - Insertion Sort", 1, draw_info.BLACK)
+    draw_info.window.blit(sorts, (draw_info.width / 2 - sorts.get_width() / 2, 35))
+
+    draw_list(draw_info)
+    pygame.display.update()
+
+def draw_list(draw_info):
+    lst = draw_info.lst
+    for i, val in enumerate(lst):
+        x = draw_info.start_x + (draw_info.block_width * i)
+        y = draw_info.height - (val - draw_info.min_val) * draw_info.block_height
+        color = draw_info.GRADIENTS[i % 3 ]
+        pygame.draw.rect(draw_info.window, color, (x, y, draw_info.block_width, draw_info.height))
 def main():
     run = True
+    n = 50
+    min_val = 1
+    max_val = 100
+    sorting = False
+    ascending = True
+    lst = generate_starting_list(n, min_val, max_val)
+    draw_info = DrawInformation(800,600, lst)
     clock = pygame.time.Clock()
     while run:
         clock.tick(60)
+        draw(draw_info)
+        pygame.display.update()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type != pygame.KEYDOWN:
+                continue
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_r:
+                    lst = generate_starting_list(n, min_val, max_val)
+                    draw_info.set_list(lst)
+                    sorting = False
+                elif event.key == pygame.K_SPACE and not sorting:
+                    sorting = True
+                elif event.key == pygame.K_a and not sorting:
+                    ascending = True
+                elif event.key == pygame.K_d and not sorting:
+                    ascending = True
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
