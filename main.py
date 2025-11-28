5111e51aeb50bca1dc8bae101a49f51ef2153239
Ashita <ashitasharma2007@gmail.com>
2025-11-28 14:10:11 +0530

    A functional ascending bubble sort!

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def main():
                 elif event.key == pygame.K_a and not sorting:
                     ascending = True
                 elif event.key == pygame.K_d and not sorting:
-                    ascending = False
+                    ascending = False #fixed this!
     pygame.quit()
 
 if __name__ == "__main__":
